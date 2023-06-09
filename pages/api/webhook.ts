51ee4c3027c330591d9a96ede6681b68e19cce51
tj <thinupavani@gmail.com>
2023-06-09 15:07:37 +0530

    update

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -18,23 +18,26 @@ interface WebhookRequestBody {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Parse the query params
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
 
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode && token === VERIFY_TOKEN) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.statusCode = 403;
-      res.end();
-    }
-  }
+  console.log("webhook is working")
+  res.status(200)
+  // Parse the query params
+  // const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  // let mode = req.query["hub.mode"];
+  // let token = req.query["hub.verify_token"];
+  // let challenge = req.query["hub.challenge"];
+
+  // // Check if a token and mode is in the query string of the request
+  // if (mode && token) {
+  //   // Check the mode and token sent is correct
+  //   if (mode && token === VERIFY_TOKEN) {
+  //     // Respond with the challenge token from the request
+  //     console.log("WEBHOOK_VERIFIED");
+  //     res.status(200).send(challenge);
+  //   } else {
+  //     // Respond with '403 Forbidden' if verify tokens do not match
+  //     res.statusCode = 403;
+  //     res.end();
+  //   }
+  // }
 }
