093ea87cd60a4f02c52b4f720fd54f47d241ef1d
tj <thinupavani@gmail.com>
2023-06-09 14:49:51 +0530

    update

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -18,78 +18,23 @@ interface WebhookRequestBody {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    // Verify the webhook
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your verification token
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
+  // Parse the query params
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
     if (mode && token === VERIFY_TOKEN) {
-      res.statusCode = 200;
-      res.end(challenge);
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
     } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
       res.statusCode = 403;
       res.end();
     }
-  } else if (req.method === 'POST') {
-    // Process incoming messages
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Replace with your verification token
-    const APP_SECRET = process.env.APP_SECRET || ''; // Replace with your app secret
-    const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN; // Replace with your page access token
-
-    const signature = req.headers['x-hub-signature'] as string;
-    const [hashMethod, hash] = signature.split('=');
-    const hmac = crypto.createHmac(hashMethod, APP_SECRET);
-    hmac.update(await req.body.toString());
-    const calculatedHash = hmac.digest('hex');
-
-    if (hash === calculatedHash) {
-      const body: WebhookRequestBody = JSON.parse(req.body);
-
-      body.entry.forEach((entry) => {
-        const { messaging } = entry;
-
-        messaging.forEach(async (event) => {
-          const senderId = event.sender.id;
-          const messageText = event.message.text;
-
-          // Add your logic here to process the incoming message and generate a response
-          // You can use the "senderId" and "messageText" variables to access the user's message
-
-          // Send a response back to the user
-          const response = {
-            recipient: {
-              id: senderId,
-            },
-            message: {
-              text: 'Hi',
-            },
-          };
-
-          try {
-            await fetch(`https://graph.facebook.com/v14.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(response),
-            });
-            console.log('Message sent successfully');
-          } catch (error) {
-            console.error('Error sending message:', error);
-          }
-        });
-      });
-
-      res.statusCode = 200;
-      res.end();
-    } else {
-      res.statusCode = 403;
-      res.end();
-    }
-  } else {
-    res.statusCode = 404;
-    res.end();
   }
 }
